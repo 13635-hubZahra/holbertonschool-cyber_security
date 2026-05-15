2826ea31d1477f76f9a0f4a51686860f038e9be4
13635-hubZahra <13635@holbertonstudents.com>
2026-05-15 21:18:33 +0000

    1. Audit SSH Configuration

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -v "^#" /etc/ssh/sshd_config | grep -v "^[[:space:]]*$"
+grep -v "^#" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf 2>/dev/null | grep -E "KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive"
