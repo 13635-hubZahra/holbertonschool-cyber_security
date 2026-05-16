d6cc470078d0795cf4773a4944b16170a65abc54
13635-hubZahra <13635@holbertonstudents.com>
2026-05-16 07:26:29 +0000

    6. Examine SMTP Server Settings

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-line=$(grep "^smtpd_tls_security_level" /etc/postfix/main.cf | grep -v "^#")
-
-if [ -z "$line" ]; then
-    echo "STARTTLS not configured"
-else
-    echo "$line" | sed 's/^[ \t]*//;s/[ \t]*$//'
-fi
+grep -q "^smtpd_tls_security_level" /etc/postfix/main.cf && grep "^smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
