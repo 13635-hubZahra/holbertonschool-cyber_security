ba51b91fee7bd77ace722c5c4a5880c7249e583f
13635-hubZahra <13635@holbertonstudents.com>
2026-06-13 17:52:51 +0000

    4. Dig it all !

--- network_security/0x01_passive_reconnaissance/4-dig_all.sh
+++ network_security/0x01_passive_reconnaissance/4-dig_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(dig $1 A +noall +answer; dig $1 NS +noall +answer; dig $1 SOA +noall +answer; dig $1 MX +noall +answer; dig $1 TXT +noall +answer) | tr '\t' ' ' | sort -u
+dig $1 +noall +answer
