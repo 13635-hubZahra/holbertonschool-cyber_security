ca084e4f258d0429224ae4031908b0e778851e81
13635-hubZahra <13635@holbertonstudents.com>
2026-06-13 18:04:31 +0000

    5. Find the sub !

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | tee /dev/stderr | while read -r sub; do ip=$(dig $sub a +short | tail -n1); [ -n "$ip" ] && echo "$sub,$ip"; done > $1.txt
+subfinder -d $1 -silent | tee /dev/stderr | while read -r sub; do ip=$(dig $sub a +short | awk 'END{print}'); [ -n "$ip" ] && echo "$sub,$ip"; done > $1.txt
