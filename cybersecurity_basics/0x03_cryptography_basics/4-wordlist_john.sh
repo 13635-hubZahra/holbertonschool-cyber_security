5d95be3284e9f05c325cddb954144bde68b9d8a6
13635-hubZahra <13635@holbertonstudents.com>
2026-06-19 18:59:13 +0000

    4. Wordlist Mode

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: 'NR>1 {print $2}' | sed '/^$/d' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=Raw-SHA256 "$1" | awk -F: 'NR>1 {print $2}' | sed '/^$/d' > 4-password.txt
